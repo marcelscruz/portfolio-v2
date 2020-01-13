db56c041c90ff95d882571fe5868705431f01312
marcelscruz <marcelcruz@live.com>
2020-01-13 19:24:17 -0300

    Fix hero image size and align content

--- src/components/hero.js
+++ src/components/hero.js
@@ -6,10 +6,15 @@ import style from '../styles/hero.module.css'
 function Hero() {
   return (
     <div className={style.hero}>
-      <div className={style.left}>I'm a web developer</div>
+      <div className={style.left}>
+        <h1>
+          I<span>'</span>m a web developer<span>.</span>
+        </h1>
+      </div>
 
       <div className={style.right}>
         <img className={style.image} src={me} alt="Marcel Cruz" />
+        <span>i</span>
       </div>
     </div>
   )
