463f2cefea3aead0a9eba172e464c9af2040acd4
marcelscruz <marcelcruz@live.com>
2020-01-23 16:37:26 -0300

    Add media-queries and theme constants

---
+++ src/constants/media-queries.js
@@ -0,0 +1,4 @@
+export default {
+    phone: 'max-width: 684px',
+    tablet: 'max-width: 900px',
+}
---
+++ src/constants/theme.js
@@ -0,0 +1,8 @@
+export default {
+    darkPrimary: '#111216',
+    darkPrimaryHex: '17, 18, 22',
+    lightPrimary: '#ffffff',
+    lightPrimaryHex: '255, 255, 255',
+    yellow: '#f4bf3a',
+    yellowHex: '244, 191, 58',
+}
