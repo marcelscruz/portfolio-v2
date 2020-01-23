ae596b2ae7d8dfb8397cdb1784bceadb41f5eabc
marcelscruz <marcelcruz@live.com>
2020-01-22 23:00:18 -0300

    Add hooks to absolute import list

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,53 +2,54 @@
 const path = require('path')
 
 module.exports = {
-  siteMetadata: {
-    title: 'marcel cruz',
-    author: 'Marcel Cruz',
-  },
-  plugins: [
-    'gatsby-plugin-styled-components',
-    {
-      resolve: 'gatsby-plugin-root-import',
-      options: {
-        src: path.join(__dirname, 'src'),
-        components: path.join(__dirname, 'src/components'),
-        layout: path.join(__dirname, 'src/layout'),
-        pages: path.join(__dirname, 'src/pages'),
-        styles: path.join(__dirname, 'src/styles'),
-      },
+    siteMetadata: {
+        title: 'marcel cruz',
+        author: 'Marcel Cruz',
     },
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: 'gatsby-source-contentful',
-      options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_DELIVERY_TOKEN,
-      },
-    },
-    'gatsby-plugin-sass',
-    'gatsby-plugin-sharp',
-    {
-      resolve: 'gatsby-transformer-remark',
-      options: {
-        plugins: [
-          'gatsby-remark-relative-images',
-          {
-            resolve: 'gatsby-remark-images',
+    plugins: [
+        'gatsby-plugin-styled-components',
+        {
+            resolve: 'gatsby-plugin-root-import',
             options: {
-              maxWidth: 750,
-              linkImagesToOriginal: false,
+                src: path.join(__dirname, 'src'),
+                components: path.join(__dirname, 'src/components'),
+                hooks: path.join(__dirname, 'src/hooks'),
+                layout: path.join(__dirname, 'src/layout'),
+                pages: path.join(__dirname, 'src/pages'),
+                styles: path.join(__dirname, 'src/styles'),
             },
-          },
-        ],
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'src',
-        path: `${__dirname}/src/`,
-      },
-    },
-  ],
+        },
+        'gatsby-plugin-react-helmet',
+        {
+            resolve: 'gatsby-source-contentful',
+            options: {
+                spaceId: process.env.CONTENTFUL_SPACE_ID,
+                accessToken: process.env.CONTENTFUL_DELIVERY_TOKEN,
+            },
+        },
+        'gatsby-plugin-sass',
+        'gatsby-plugin-sharp',
+        {
+            resolve: 'gatsby-transformer-remark',
+            options: {
+                plugins: [
+                    'gatsby-remark-relative-images',
+                    {
+                        resolve: 'gatsby-remark-images',
+                        options: {
+                            maxWidth: 750,
+                            linkImagesToOriginal: false,
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'src',
+                path: `${__dirname}/src/`,
+            },
+        },
+    ],
 }
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,27 +1,27 @@
 const path = require('path')
 
 module.exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions
-  const postTemplate = path.resolve('./src/components/post/index.js')
-  const res = await graphql(`
-    query {
-      allContentfulBlogPost {
-        edges {
-          node {
-            slug
-          }
+    const { createPage } = actions
+    const postTemplate = path.resolve('./src/components/post/index.js')
+    const res = await graphql(`
+        query {
+            allContentfulBlogPost {
+                edges {
+                    node {
+                        slug
+                    }
+                }
+            }
         }
-      }
-    }
-  `)
+    `)
 
-  res.data.allContentfulBlogPost.edges.forEach(edge => {
-    createPage({
-      component: postTemplate,
-      path: `/${edge.node.slug}`,
-      context: {
-        slug: edge.node.slug,
-      },
+    res.data.allContentfulBlogPost.edges.forEach(edge => {
+        createPage({
+            component: postTemplate,
+            path: `/${edge.node.slug}`,
+            context: {
+                slug: edge.node.slug,
+            },
+        })
     })
-  })
 }
