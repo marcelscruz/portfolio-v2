b31c1c9577370852bf83742a37f8bf08adbe3494
marcelscruz <marcelcruz@live.com>
2020-02-04 21:41:23 -0300

    Add custom hooks

--- src/hooks/useMedia.js
+++ src/hooks/useMedia.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 
-function useMedia(queries, values, defaultValue) {
+export function useMedia(queries, values, defaultValue) {
     // Array containing a media query list for each query
     const mediaQueryLists = queries.map(q => window.matchMedia(q))
 
