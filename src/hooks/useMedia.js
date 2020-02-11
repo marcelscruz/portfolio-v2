f95cee6561c3c648c9ca20b8b93e53e6b049fa43
marcelscruz <marcelcruz@live.com>
2020-02-10 21:23:56 -0300

    Fix undefined window

--- src/hooks/useMedia.js
+++ src/hooks/useMedia.js
@@ -2,7 +2,9 @@ import { useEffect, useState } from 'react'
 
 export function useMedia(queries, values, defaultValue) {
     // Array containing a media query list for each query
-    const mediaQueryLists = queries.map(q => window.matchMedia(q))
+    const mediaQueryLists = queries.map(
+        q => typeof window !== 'undefined' && window.matchMedia(q),
+    )
 
     // Function that gets value based on matching media query
     const getValue = () => {
