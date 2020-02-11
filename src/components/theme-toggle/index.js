233d4d26521603874de282c4c1e8ec5d39dbbe01
marcelscruz <marcelcruz@live.com>
2020-02-10 22:54:45 -0300

    Fix wrong initial theme icon

--- src/components/theme-toggle/index.js
+++ src/components/theme-toggle/index.js
@@ -1,23 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { IconWrapper, MoonOrSun, MoonMask } from './theme-toggle.styles'
 
 const ThemeToggle = ({ isDarkModeOn, setIsDarkModeOn }) => {
+    const [isOn, setIsOn] = useState(true)
+
+    // Used to force the icon to update it's initial state.
+    // Without it the icon starts in the light mode (moon)
+    // even if dark mode is set. ¯\_(ツ)_/¯
+    useEffect(() => {
+        setIsOn(isDarkModeOn)
+    }, [isDarkModeOn])
+
     function toggleColorMode() {
         setIsDarkModeOn(!isDarkModeOn)
     }
 
     return (
         <IconWrapper
-            isDarkModeOn={isDarkModeOn}
+            isDarkModeOn={isOn}
             onClick={toggleColorMode}
             data-a11y="false"
-            aria-label={
-                isDarkModeOn ? 'Activate light mode' : 'Activate dark mode'
-            }
+            aria-label={isOn ? 'Activate light mode' : 'Activate dark mode'}
         >
-            <MoonOrSun isDarkModeOn={isDarkModeOn} />
-            <MoonMask isDarkModeOn={isDarkModeOn} />
+            <MoonOrSun isDarkModeOn={isOn} />
+            <MoonMask isDarkModeOn={isOn} />
         </IconWrapper>
     )
 }
